e35f1d30bea44425e75072b849078991213efbe2
Jamie Rodriguez <65564846+fivetran-jamie@users.noreply.github.com>
2020-08-26 13:00:12 -0700

    Create bigquery_daily_records_modified.sql

---
+++ bigquery_daily_records_modified.sql
@@ -0,0 +1,15 @@
+SELECT
+  date_trunc(cast(time_stamp as date), day) AS date_day,
+  JSON_EXTRACT(message_data, '$.schema') as schema, 
+  JSON_EXTRACT(message_data, '$.table') as table, 
+  SUM(cast(JSON_EXTRACT(message_data, '$.count') as int64)) as records_updated_or_inserted
+
+FROM fivetran_log.log
+
+WHERE 
+  date_diff( cast(CURRENT_DATE() as date), cast(time_stamp as date), DAY) < 30
+  AND message_event = 'records_modified'
+  and JSON_EXTRACT_SCALAR(message_data, '$.operationType') = 'REPLACED_OR_INSERTED'
+
+group by date_day, schema, table
+order by date_day desc
