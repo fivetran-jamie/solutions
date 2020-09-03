028f7e43f8b782d09b28ec461bfec7af5885972b
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2020-09-03 16:46:03 -0700

    since sync end

--- fivetran_docs_queries/last_records_modified_at/snowflake_latest_records_modified.sql
+++ fivetran_docs_queries/last_records_modified_at/snowflake_latest_records_modified.sql
@@ -1,27 +1,21 @@
 WITH parse_json AS (
     SELECT
         time_stamp,
-        PARSE_JSON(message_data) AS message_data
+        PARSE_JSON(message_data) AS message_data,
+        message_event,
+        MAX(CASE WHEN message_event = 'sync_end' THEN time_stamp ELSE NULL END) OVER(PARTITION BY connector_id) AS last_sync_completed_at
     FROM fivetran_log.log
     WHERE message_event = 'records_modified'
-),
-
-order_logs as (
-    SELECT
-        time_stamp,
-        message_data:schema AS connector_schema, 
-        message_data:count::integer AS row_volume,
-        DENSE_RANK() OVER(PARTITION BY message_data:schema 
-            ORDER BY DATE_TRUNC('MINUTE', time_stamp) DESC) AS nth_last_record
-    FROM parse_json
+        OR message_event = 'sync_end'
 )
 
 select 
-    connector_schema,
+    message_data:schema as connector_schema,
     MAX(time_stamp) AS last_records_modified_at,
-    SUM(row_volume) AS row_volume
-FROM order_logs
-WHERE nth_last_record = 1
+    SUM(message_data:count::integer) AS row_volume
+FROM parse_json 
+WHERE message_event = 'records_modified'
+    AND time_stamp > last_sync_completed_at
 GROUP BY connector_schema
 ORDER BY last_records_modified_at DESC
 ;
