263e4b32fb81ad2b21746b865bf7a76648ebefff
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2021-03-24 16:00:23 -0700

    mar by month by shcema

---
+++ fivetran_docs_queries/mar_by_month_schema.sql
@@ -0,0 +1,20 @@
+with latest_measurements as (
+    select
+        schema_name,
+        table_name,
+        destination_id,
+        measured_at,
+        to_char(measured_at, 'YYYY-MM') as measured_month,
+        monthly_active_rows,
+        row_number() over(partition by table_name, schema_name, destination_id, to_char(measured_at, 'YYYY-MM') order by measured_at desc) as n
+    from <fivetran_load_database>.<fivetran_log_schema_name>.active_volume 
+)
+
+select
+    schema_name,
+    destination_id,
+    measured_month,
+    sum(monthly_active_rows) as MAR
+from latest_measurements
+group by schema_name, destination_id, measured_month
+order by measured_month, schema_name
